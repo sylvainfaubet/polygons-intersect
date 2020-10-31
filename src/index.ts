3dcbb7022aca39234f455a281d9f9b880f78c497
sylvain faubet <faubet.sylvain@gmail.com>
2020-10-31 22:15:23 +0100

    update of librairies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import { Point } from "./model/point";
-import { Polygon } from "./model/polygon";
-import intersect from "./services/intersection";
+export * from "./model/point";
+export * from "./model/polygon";
+export * as intersect from "./services/intersection";
 
-export default (poly1, poly2) => {
-    const p1 = new Polygon(poly1.map(p => new Point(p.x, p.y)));
-    const p2 = new Polygon(poly2.map(p => new Point(p.x, p.y)));
-    const polygons = intersect(p1, p2);
-    return polygons.map(poly => poly.getPath());
-};
